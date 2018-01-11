a14be71a1e207a1d00bf649266b196dd953397eb
1617193116甘曾盛 <33241866+ganzengsheng@users.noreply.github.com>
2018-01-11 08:55:23 +0800

    Add files via upload

---
+++ 爬取职位信息/2.py
@@ -0,0 +1,18 @@
+import urllib
+import urllib2
+import re
+import thread
+import time
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.108 Safari/537.36 2345Explorer/8.8.3.16721'
+pageStories = []
+url = 'http://www.51jrjob.com/jobseeker/stage/findjob.html?kv=&cv=&acv=&fl=&it=&le=&page=3'
+headers = {'User-Agent':user_agent}
+result = urllib2.Request( url, headers=headers )
+response = urllib2.urlopen( result )
+pageCode = response.read().decode('utf-8')
+pattern = re.compile(r'<a class="pos-detail".*?>(.*?)</a>.*?<a.*?>(.*?)</a>.*?<span class="spansalary">(.*?)</span>',re.S )
+items = re.findall( pattern, pageCode )
+for i in items:
+    for g in i:
+        print g
